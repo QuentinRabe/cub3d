233265de2dc4f5a0638f2962a8745822927d2f80
QuentinRabe <lovegoodandyh@gmail.com>
2025-01-19 09:59:42 +0300

    (parsing): check if texture paths exist

--- srcs/parsing/texture.c
+++ srcs/parsing/texture.c
@@ -6,13 +6,53 @@
 /*   By: arabefam <arabefam@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 14:15:16 by arabefam          #+#    #+#             */
-/*   Updated: 2025/01/16 15:15:57 by arabefam         ###   ########.fr       */
+/*   Updated: 2025/01/19 09:57:47 by arabefam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
-void	check_id(char *id, int *bits)
+static void	try_to_open_file(char *content, char **contents, \
+size_t index, int *count)
+{
+	char	*filename;
+	size_t	len;
+	int		fd;
+
+	len = ft_strlen_charset(content + index, '\n');
+	filename = ft_substr(content, index, len);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		print_error("One or more specified texture paths do not exist.\
+ Please verify the paths and try again.");
+		close(fd);
+		free_array(contents);
+		free(filename);
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+	(*count)++;
+	free(filename);
+}
+
+static void	check_texture_path(char *content, char **contents)
+{
+	size_t		index;
+	static int	count = 0;
+
+	if (count < 4)
+	{
+		index = ft_strlen_set(content, SPACES) + 1;
+		if (index == 2)
+			return ;
+		try_to_open_file(content, contents, index, &count);
+	}
+	if (count == 4)
+		count = 0;
+}
+
+static void	check_id(char *id, int *bits)
 {
 	if (!ft_strcmp(id, "NO"))
 		*bits |= 1 << 5;
@@ -28,7 +68,7 @@ void	check_id(char *id, int *bits)
 		*bits |= 1;
 }
 
-void	check_valid_identifier(char *content, char **contents)
+static void	check_valid_identifier(char *content, char **contents)
 {
 	char		*id;
 	size_t		len;
@@ -45,7 +85,8 @@ void	check_valid_identifier(char *content, char **contents)
 		if (bits != 63)
 		{
 			print_error("The file contains one or more invalid identifiers.\
- Valid identifiers are: NO, EA, SO, WE, F, C. Please check them and ensure all identifiers are unique and valid.");
+ Valid identifiers are: NO, EA, SO, WE, F, C.\
+ Please check them and ensure all identifiers are unique and valid.");
 			free_array(contents);
 			exit(EXIT_FAILURE);
 		}
@@ -66,6 +107,7 @@ void	parse_texture(char **contents)
 		if (is_only(SPACES, contents[i]))
 			continue ;
 		check_valid_identifier(contents[i], contents);
+		check_texture_path(contents[i], contents);
 		count++;
 	}
 }
